fc1d7a157239949be255ad24c82e8aa46d68a969
Andrii Vasylenko <vasilenkoforwork@gmail.com>
2024-08-12 18:16:59 +0200

    implementation of new scenario for search

--- pages/search-results.ts
+++ pages/search-results.ts
@@ -2,10 +2,15 @@ import { Page, expect } from "@playwright/test";
 
 export class SearchResultsPage {
   private readonly arsenalInSearchesults: string = '//a[@title="Arsenal FC"]';
+  private readonly UEFAChampionsLeagueInSearchesults: string = '//a[@title="UEFA Champions League"]';
 
   constructor(private readonly page: Page) {}
 
   public async clickOnTheFirstTeam() {
     await this.page.locator(this.arsenalInSearchesults).first().click();
+  }
+
+  public async clickOnTheChampionsLeagueCompetition() {
+    await this.page.locator(this.UEFAChampionsLeagueInSearchesults).first().click();
   }
 }
